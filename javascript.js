1aabd03593b63882384fed94a4a82243e08052fa
MinJin Park <parkmin1986@gmail.com>
2020-01-18 20:26:26 -0500

    5day is working

--- javascript.js
+++ javascript.js
@@ -6,6 +6,7 @@ var currentDate = now.format('l');
 var APIKey = "2bca8808676f3776bfbc587d4a852ac4";
 
 function cityInfo(city) {
+  localStorage.setItem("city", city)
   // Here we are building the URL we need to query the database
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
   // Here we run our AJAX call to the OpenWeatherMap API
@@ -26,16 +27,22 @@ function cityInfo(city) {
       $(".city-text").html("<h3>" + response.name + " " + "(" + currentDate + ")" + "</h3>");
       $("#wind").text("Wind Speed: " + response.wind.speed);
       $("#humidity").text("Humidity: " + response.main.humidity);
-      $("#temp").text("Temperature (F) " + response.main.temp);
+      $("#temp").text("Temperature (F): " + response.main.temp);
 
       var lat = response.coord.lat;
       var lon = response.coord.lon;
       uvIndex(lat, lon);
+      fiveDays(city)
 
       var iconCode = response.weather[0].icon;
       var iconURL = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-      var icon = $("#wicon").attr("src", iconURL);
+      $("#wicon").attr("src", iconURL);
     });
+}
+
+function init () {
+  var city = localStorage.getItem("city") || "Atlanta"
+  cityInfo(city)
 }
 
 function uvIndex(lat, lon) {
@@ -66,6 +73,36 @@ function uvIndex(lat, lon) {
       }
     });
 };
+
+function fiveDays(city) {
+  var queryFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
+
+
+  $.ajax({
+    url: queryFive,
+    method: "GET"
+  })
+
+  .then(function(response){
+    console.log(response.list)
+    for (var i = 0; i < 40; i += 8) {
+      console.log(response.list[i].main.temp)
+      $('#temp' + i).text("Temp: " + response.list[i].main.temp)
+      console.log(response.list[i].main.humidity)
+      $("#humidity" + i).text("Hu: " + response.list[i].main.humidity)
+      console.log(response.list[i].weather[0].icon)
+      var iconCode = response.list[i].weather[0].icon;
+      var iconURL = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+      $("#wicon" + i).attr("src", iconURL);
+
+      console.log(response.list[i].dt_txt)
+      $("#date" + i).text(moment(response.list[i].dt_txt).format("l"))
+
+
+
+    }
+  })
+}
 
 
 
@@ -98,3 +135,5 @@ $(document).on("click", ".city", function () {
   var city = $(this).attr("data-name");
   cityInfo(city);
 });
+
+init();
